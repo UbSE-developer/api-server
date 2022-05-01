acc14ba364492e15ed9d47708332816bcd56f48a
LeeHyeonHee <ddrtocna@gmail.com>
2022-05-01 13:08:10 +0900

    fixed SWAGGER err

--- node/src/app.ts
+++ node/src/app.ts
@@ -8,48 +8,45 @@ import 'dotenv/config';
 import cors from 'cors';
 import { DEFAULT_PORT, DEFAULT_URL, DEFAULT_VERSION, SWAGGER_URL } from './common/const';
 
-const main = async () => {
-    const port = DEFAULT_PORT;
+const port = DEFAULT_PORT;
 
-    const options: cors.CorsOptions = {
-      allowedHeaders: [
-          'Origin',
-          'X-Requested-With',
-          'Content-Type',
-          'Accept',
-          'X-Access-Token',
-      ],
-      credentials: true,
-      methods: 'GET,POST',
-      origin: DEFAULT_URL,
-      preflightContinue: false,
-  };
-
-    const swaggerOptions = {
-        swaggerDefinition: {
-            info: {
-                title: 'ubsta-Server',
-                version: '1.0.0',
-                description: 'ubsta backend server',
-            },
-            host: '192.168.0.14:4000/',
-            basePath: 'v1',
-        },
-        apis: ['swagger.yaml'],
-    };
-
-    const specs = swaggerJsDoc(swaggerOptions);
-
-    const app = express();
-
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-    app.use(bodyParser.json());
-    app.use(cors(options));
-    app.use('/v1', RootRouter);
-
-
-    app.listen(port, async () => {
-      console.log('Server Start');
-    });
+const options: cors.CorsOptions = {
+    allowedHeaders: [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,POST',
+    origin: DEFAULT_URL,
+    preflightContinue: false,
 };
-main();
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'ubsta-Server',
+            version: '1.0.0',
+            description: 'ubsta backend server',
+        },
+        host: SWAGGER_URL,
+        basePath: DEFAULT_VERSION,
+    },
+    apis: ['swagger.yaml'],
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
+
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(bodyParser.json());
+app.use(cors(options));
+app.use('/v1', RootRouter);
+
+
+app.listen(port, async () => {
+    console.log('Server Start');
+});
--- node/src/common/const.ts
+++ node/src/common/const.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-export const DEFAULT_PORT = process.env.PORT || '4000';
-export const DEFAULT_URL = process.env.URL + '192.168.0.14';
-export const SWAGGER_URL = process.env.URL + ':' + process.env.PORT || 'http://192.168.0.14:4000';
+export const DEFAULT_PORT = '4000';
+export const DEFAULT_URL = 'http://192.168.0.15';
+export const SWAGGER_URL = '192.168.0.15:4000';
 export const DEFAULT_VERSION = process.env.VERSION || 'v1';
